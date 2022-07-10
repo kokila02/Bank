b3b8eb9b9d069000e4d6ecb1091b2a6775828a17
kokila02 <Kaarthikdhanushka2004@gmail.com>
2022-07-10 12:21:10 +0530

    new branch

--- src/org/test/AxisBank.java
+++ src/org/test/AxisBank.java
@@ -3,9 +3,11 @@ package org.test;
 public class AxisBank {
 private void Savings() {
 System.out.println("5%");
+System.out.println("7%");
 }
 private void fixed() {
 System.out.println("7%");
+System.out.println("8%");
 }
 public static void main(String[] args) {
 	AxisBank a=new AxisBank();
