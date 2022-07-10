d91374b3cc40cdd0e1d5cdde01ba022620dd6377
kokila02 <Kaarthikdhanushka2004@gmail.com>
2022-07-10 12:11:45 +0530

    code

---
+++ src/org/test/AxisBank.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class AxisBank {
+private void Savings() {
+System.out.println("5%");
+}
+private void fixed() {
+System.out.println("7%");
+}
+public static void main(String[] args) {
+	AxisBank a=new AxisBank();
+	a.fixed();
+	a.Savings();
+}
+}
